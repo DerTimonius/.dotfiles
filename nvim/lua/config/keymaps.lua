b610a991055f6e96a4c6b13ec09f06cfe7f224ef
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-11-27 13:08:30 +0100

    some remaps, options

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "<C-c>", "Esc")
-vim.keymap.set("v", "<C-c>", "Esc")
+local set = vim.keymap.set
 
-vim.keymap.set("i", "<C-s>", "<esc><cmd>w<CR>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+set("i", "<C-c>", "Esc")
+set("v", "<C-c>", "Esc")
+
+set("i", "<C-s>", "<esc><cmd>w<CR>")
+set("n", "<leader>pv", vim.cmd.Ex)
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+
+set("x", "<leader>p", '"_dP')
+set("n", "<leader>y", '"+y')
+set("v", "<leader>y", '"+y')
+set("n", "<leader>cs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.scrolloff = 10
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
