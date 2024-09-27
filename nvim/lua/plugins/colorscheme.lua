f202753037261bb612bf21ca422c7989f740a70a
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-09-27 08:41:53 +0200

    add more themes, themery to nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -51,9 +51,65 @@ return {
     },
   },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    "yorumicolors/yorumi.nvim",
+  },
+  { "diegoulloao/neofusion.nvim" },
+  {
+    "olivercederborg/poimandres.nvim",
+    config = function()
+      local p = require("poimandres.palette")
+      require("poimandres").setup({
+        highlight_groups = {
+          LspReferenceText = { bg = p.background1 },
+          LspReferenceRead = { bg = p.background1 },
+          LspReferenceWrite = { bg = p.background1 },
+        },
+      })
+    end,
+  },
+  {
+    "zaldih/themery.nvim",
+    lazy = true,
+    config = function()
+      require("themery").setup({
+        livePreview = true,
+        themes = { "catppuccin", "yorumi", "neofusion", "poimandres", "tokyonight" },
+      })
+    end,
+    keys = {
+      { "<leader>tt", "<cmd>Themery<CR>", desc = "Open theme switcher" },
+      {
+        "<leader>tc",
+        function()
+          local themery = require("themery")
+          themery.setThemeByName("catppuccin", true)
+        end,
+        desc = "Switch theme to catppuccin",
+      },
+      {
+        "<leader>ty",
+        function()
+          local themery = require("themery")
+          themery.setThemeByName("yorumi", true)
+        end,
+        desc = "Switch theme to yorumi",
+      },
+      {
+        "<leader>tn",
+        function()
+          local themery = require("themery")
+          themery.setThemeByName("neofusion", true)
+        end,
+        desc = "Switch theme to neofusion",
+      },
+      {
+        "<leader>tp",
+        function()
+          local themery = require("themery")
+          themery.setThemeByName("poimandres", true)
+        end,
+        desc = "Switch theme to poimandres",
+      },
     },
   },
 }
